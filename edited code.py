8dc64979bffc5b06aed956cfda6be852fca7541c
DADDY698 <mdshameeljubayer@gmail.com>
2024-10-15 16:06:29 +0800

    edited code.py

--- edited code.py
+++ edited code.py
@@ -131,3 +131,103 @@ def delete_customer():
 # --- manage customers function ends ---
 
 # --- manage menu function starts ---
+def manage_menu():
+    while True:
+        print("\n~~~~~~ Manage Menu Categories and Pricing ~~~~~~")
+        print("1. Add Menu Item")
+        print("2. Edit Menu Item")
+        print("3. Delete Menu Item")
+        print("4. Back to Manager Menu")
+        selection = input("Select an option: ")
+
+        match selection:
+            case "1":
+                add_menu_item()
+            case "2":
+                edit_menu_item()
+            case "3":
+                delete_menu_item()
+            case "4":
+                break
+            case _:
+                print("Invalid input. Please try again!")
+
+# Add menu item function
+def add_menu_item():
+    print("Enter the details to add menu item: ")
+    category = input("Enter category: ")
+    item_name = input("Enter item name: ")
+    price = float(input("Enter price: "))
+    with open("menu.txt", "a") as file:
+        file.write(f"{category},{item_name},{price}\n")
+    print("Menu item added successfully!")
+
+# Edit menu item function
+def edit_menu_item():
+    item_name = input("Enter the name of the menu item to edit: ")
+    data = []
+    found = False
+
+    with open("menu.txt", "r") as file:
+        for line in file:
+            info = line.strip().split(",")
+            if info[1] == item_name:
+                found = True
+                print("\nWhat would you like to edit?")
+                print("1. Category")
+                print("2. Item Name")
+                print("3. Price")
+                print("4. Back")
+
+                selection = int(input("Choose the option you want to edit: "))
+
+                match selection:
+                    case 1:
+                        info[0] = input("\nEnter the new category: ")
+                    case 2:
+                        info[1] = input("\nEnter the new item name: ")
+                    case 3:
+                        info[2] = float(input("\nEnter the new price: "))
+                    case 4:
+                        print("Back to menu\n")
+                        return
+                    case _:
+                        print("Invalid selection! Please choose a valid option.")
+
+            data.append(info)
+
+    with open("menu.txt", "w") as file:
+        for line in data:
+            file.write(f"{line[0]},{line[1]},{line[2]}\n")
+
+    if found:
+        print("Menu item edited successfully!")
+    else:
+        print("No matching menu item found.")
+
+# Delete menu item function
+def delete_menu_item():
+    cat = input("Enter the name of the catergory: ")
+    item_name = input("Enter the name of the menu item to delete: ")
+    data = []
+    found = False
+
+    with open("menu.txt", "r") as file:
+        for line in file:
+            info = line.strip().split(",")
+            if info[1] == item_name and info[0] == cat:
+                found = True
+            else:
+                data.append(info)
+
+    with open("menu.txt", "w") as file:
+        for line in data:
+            file.write(f"{line[0]},{line[1]},{line[2]}\n")
+
+    if found:
+        print("Menu item deleted successfully!")
+    else:
+        print("No matching menu item found.")
+
+# --- manage menu function ends ---        
+# --- manage menu function starts ---
